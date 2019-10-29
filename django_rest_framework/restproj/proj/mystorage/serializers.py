c4faa60a96a08f2b4905d7333091cb279f56c13a
Yurim Seo <yrseo1216@gmail.com>
2019-10-29 13:35:13 +0900

    finish django api project

--- django_rest_framework/restproj/proj/mystorage/serializers.py
+++ django_rest_framework/restproj/proj/mystorage/serializers.py
@@ -1,10 +1,29 @@
-from .models import Essay
+from .models import Essay, Album, Files
 from rest_framework import serializers
 
 class EssaySerializer(serializers.ModelSerializer):
 
-    author_name = serializers.ReadOnlyField(source='author.username')
+    author = serializers.ReadOnlyField(source='author.username')
 
     class Meta:
         model = Essay
-        fields = ('pk', 'title', 'body', 'author_name')
+        fields = ('pk', 'title', 'body', 'author')
+
+class AlbumSerializer(serializers.ModelSerializer):
+
+    author = serializers.ReadOnlyField(source='author.username')
+    image = serializers.ImageField(use_url=True)
+
+    class Meta:
+        model = Album
+        fields = ('pk', 'author', 'image','desc')
+
+class FileSerializer(serializers.ModelSerializer):
+
+    author = serializers.ReadOnlyField(source='author.username')
+    myfiles = serializers.FileField(use_url=True)
+
+    class Meta:
+        model = Files
+        fields = ('pk','author', 'myfiles','desc')
+
--- django_rest_framework/restproj/proj/mystorage/views.py
+++ django_rest_framework/restproj/proj/mystorage/views.py
@@ -1,7 +1,10 @@
 from rest_framework import viewsets
-from .models import Essay
-from .serializers import EssaySerializer
+from .models import Essay, Album, Files
+from .serializers import EssaySerializer, AlbumSerializer, FileSerializer
 from rest_framework.filters import SearchFilter
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
+from rest_framework import status
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Essay.objects.all()
@@ -9,6 +12,7 @@ class PostViewSet(viewsets.ModelViewSet):
 
     filter_backends = [SearchFilter]
     search_fields = ('title','body')
+
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
     
@@ -23,3 +27,27 @@ class PostViewSet(viewsets.ModelViewSet):
         else:
             qs = qs.none()
         return qs
+
+class ImageViewSet(viewsets.ModelViewSet):
+    queryset = Album.objects.all()
+    serializer_class = AlbumSerializer
+
+class FileViewSet(viewsets.ModelViewSet):
+    queryset = Files.objects.all()
+    serializer_class = FileSerializer
+
+    # parser_class 지정
+    # create() 오버라이딩
+    parser_classes = (MultiPartParser, FormParser)
+    #다양한 미디어 형태들을 수락할 수 있게 도와준다
+
+    # API HTTP -> get() post()
+    # create() --> post() 요청
+
+    def post(self, request, *args, **kwargs):
+        serializer = FileSerializer(data = request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=HTTP_201_CREATED)
+        else:
+            return Response(serializer.error, status=HTTP_400_BAD_REQUEST)
